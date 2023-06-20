a55219f89d717e7a2962ed962c1098ce8b678189
SavioHaistem <savio.dantas.5858@gmail.com>
2023-06-20 10:59:15 -0300

    Melhorando hook

---
+++ src/Components/Hooks/useApi.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+
+function useApi(method: string, url: string, configs: object = {}): any {
+  const UpdateApi =async()=> {
+    
+  };
+
+  const DeleteInApi =async()=> {
+
+  };
+
+  const GetApi =async()=> {
+    let response = await axios.get(`${url}`);
+    return response.data
+  };
+
+  const PostApi =async()=> {
+    return await axios.post(`${url}`, configs);
+  };
+
+  return method === 'POST' ? PostApi() : 
+  method === 'GET' ? GetApi() :
+  method === 'DELETE' ? DeleteInApi() :
+  method === 'UPDATE' ? UpdateApi() : 'No method'
+
+};
+
+export default useApi;
