9e1e78671023f7b14d0b3f8823a29879a0672436
SavioHaistem <savio.dantas.5858@gmail.com>
2023-06-23 16:06:44 -0300

    Adicionando tema básico do formulário

--- src/Styles/Forms.tsx
+++ src/Styles/Forms.tsx
@@ -1,12 +1,22 @@
 import { styled } from "styled-components";
 
 const UserForm = styled.form`
-  height: 10vh;
-  width: 20vw;
-  background-color: #f9a9fe;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+  width: 50vw;
+  background-color: ${({theme})=>theme.colors.backgroundPrimary};
+  border-radius: 1rem;
 `
 const TextFormInput = styled.input`
-  
+  border: none;
+  border-radius: 10px;
+  padding: 0.5rem;
+  margin: 3px 0px;
+  width: 20rem;
+  height: 2rem;
 `
 
 export { UserForm, TextFormInput }
